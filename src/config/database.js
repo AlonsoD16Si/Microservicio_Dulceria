47e04953fb77cd53e598d9f52ca52dbeffb53b83
AlonsoD16Si <alonso_lopezsilva@live.com>
2024-11-28 18:37:12 -0600

    database modificado

--- src/config/database.js
+++ src/config/database.js
@@ -4,8 +4,14 @@ const { DB_NAME, DB_USER, DB_PASS, DB_HOST } = require('./environment');
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
   host: DB_HOST,
   dialect: 'mysql',
-  port:process.env.DB_PORT || 3306,
-  logging: false
+  port: process.env.DB_PORT || 3306,
+  logging: false,
+  dialectModule: require('mysql2'), // Añade esta línea
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: true // Añade soporte para conexiones SSL
+    }
+  }
 });
 
 module.exports = sequelize;
